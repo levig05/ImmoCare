48da85d042dce89370f13c5a867b46bb0c32ca02
PhilipLaibKFTG <122262598+PhilipLaibKFTG@users.noreply.github.com>
2023-04-20 10:08:57 +0200

    Immobilien Übersicht: Übersicht erstellen: 7: Overview Component erstellt
    Der Component Overview wurde erstellt und verbunden

--- ImmobilienWebseite/client/src/App.js
+++ ImmobilienWebseite/client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import Overview from "./components/overview/Overview";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Overview />
     </div>
   );
 }
---
+++ ImmobilienWebseite/client/src/components/overview/Overview.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./overview.css";
+
+function Overview() {
+  return <div></div>;
+}
+
+export default Overview;
