5c38227bf152cbdc5fd144d3379d77844609fc81
PhilipLaibKFTG <122262598+PhilipLaibKFTG@users.noreply.github.com>
2023-04-20 09:42:58 +0200

    Immobilien Übersicht: Übersicht erstellen: 4: Header Titel wurde erstellt
    Titel wurde eingefügt

--- ImmobilienWebseite/client/src/components/header/Header.jsx
+++ ImmobilienWebseite/client/src/components/header/Header.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./header.css"
 
 function Header() {
-    return <div className="App"></div>;
+    return <div className="Header">
+        <h1 className="title">ImmoCare</h1>
+        <div className="search">
+            
+        </div>
+    </div>;
   }
   
   export default Header;
