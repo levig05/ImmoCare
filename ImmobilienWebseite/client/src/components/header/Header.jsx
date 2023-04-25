5126fabc164f3a94f69d0714187a7bdcc3156ef9
PhilipLaibKFTG <122262598+PhilipLaibKFTG@users.noreply.github.com>
2023-04-25 14:36:36 +0200

    Immobilien-Übersicht: Übersicht erstellen: 10
    Beispiel für Immobilien Ansicht erstellt. Nächster Schritt wäre js function für das Erstellen einer neuen Card für jede Immobilie und einfügen der Optionen.

--- ImmobilienWebseite/client/src/components/header/Header.jsx
+++ ImmobilienWebseite/client/src/components/header/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import "./header.css"
+import "./header.css";
 
 function Header() {
-    return <div className="Header">
-        <h1 className="title">ImmoCare</h1>
-        <div className="search">
-            
-        </div>
-    </div>;
-  }
-  
-  export default Header;
+  return (
+    <div className="Header">
+      <h1 className="title">ImmoCare</h1>
+      <div className="search">
+        <hr />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- ImmobilienWebseite/client/src/components/overview/Overview.jsx
+++ ImmobilienWebseite/client/src/components/overview/Overview.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./overview.css";
+import testimmo from "../assets/testimmo.jpeg";
 
 function Overview() {
-  return <div></div>;
+  return (
+    <div className="overview">
+      <div class="card">
+        <img src={testimmo} alt="Immobilie" width="100%"></img>
+        <div class="container">
+          <h4>
+            <b>Wohnhaus 1</b>
+          </h4>
+          <p>Beispieladresse 123</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Overview;
