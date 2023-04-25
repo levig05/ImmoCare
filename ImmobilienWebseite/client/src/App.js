32ba64583e04a9e084a3f9cc7d73abf1b044b52d
PhilipLaibKFTG <122262598+PhilipLaibKFTG@users.noreply.github.com>
2023-04-25 15:40:17 +0200

    Immobilien Hinzufügen: Eingabefelder Aufsetzen: 13
    Propertyform wird in App.js aufgerufen

--- ImmobilienWebseite/client/src/App.js
+++ ImmobilienWebseite/client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Overview from "./components/overview/Overview";
+import PropertyForm from "./Reac";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Overview />
+      <PropertyForm />
     </div>
   );
 }
